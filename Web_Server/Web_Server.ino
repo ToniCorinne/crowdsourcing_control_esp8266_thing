f6f223583aa46a9adab038fd35722027ee785d24
Toni Klopfenstein <toni.klopfenstein@gmail.com>
2015-11-06 12:44:53 -0700

    Adding web server example

---
+++ Web_Server/Web_Server.ino
@@ -0,0 +1,137 @@
+/******************************************************************************
+HackaDay Super Conference Blink_Hackaday
+Toni Klopfenstein @ SparkFun Electronics
+OCtober 2015
+<github repository address>
+
+Intro sketch to test basic features of the ESP8266 Thing.
+
+Resources:
+ESP8266 Arduino Add-On Required
+
+Development environment specifics:
+Arduino 1.6.5+ with the ESP8266 Thing()
+
+Based on AP Web Server example by Jim Lindblom, SparkFun Electronics 2015
+
+This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+Please review the LICENSE.md file included with this example. If you have any questions 
+or concerns with licensing, please contact techsupport@sparkfun.com.
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#include <ESP8266WiFi.h>
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char WiFiAPPSK[] = "sparkfun";
+
+/////////////////////
+// Pin Definitions //
+/////////////////////
+
+const int ESP8266_Analog = A0;
+const int ESP8266_LED = 4;
+
+
+WiFiServer server(80);
+
+void setup() 
+{
+  initHardware();
+  setupWiFi();
+  server.begin();
+}
+
+void loop() 
+{
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  // Match the request
+  int val = -1; // We'll use 'val' to keep track of both the
+                // request type (read/set) and value if set.
+  if (req.indexOf("/led/0") != -1)
+    val = 0; // Will write LED low
+  else if (req.indexOf("/led/1") != -1)
+    val = 1; // Will write LED high
+  else if (req.indexOf("/read") != -1)
+    val = -2; // Will print pin reads
+  // Otherwise request will be invalid. We'll say as much in HTML
+
+  // Set GPIO5 according to the request
+  if (val >= 0)
+    digitalWrite(ESP8266_LED, val);
+
+  client.flush();
+
+  // Prepare the response. Start with the common header:
+  String s = "HTTP/1.1 200 OK\r\n";
+  s += "Content-Type: text/html\r\n\r\n";
+  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  // If we're setting the LED, print out a message saying we did
+  if (val >= 0)
+  {
+    s += "LED is now ";
+    s += (val)?"on":"off";
+  }
+  else if (val == -2)
+  { // If we're reading pins, print out those values:
+    s += "Analog Pin = ";
+    s += String(analogRead(ESP8266_Analog));
+  }
+  else
+  {
+    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
+  }
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
+void initHardware()
+{
+  Serial.begin(115200);
+  pinMode(ESP8266_LED, OUTPUT);
+  digitalWrite(ESP8266_LED, LOW);
+}
+
+
